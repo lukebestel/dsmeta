d1f981b732548e181417d0d1dd42a97de5528cd8
Lucas Bestel <lukebestel@gmail.com>
2022-09-07 17:40:14 -0300

    Security config

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -14,7 +14,7 @@ function SalesCard() {
 
     return (
         <div className="dsmeta-card">
-            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <h2 className="dsmeta-sales-title">Sales</h2>
             <div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
@@ -39,19 +39,19 @@ function SalesCard() {
                     <thead>
                         <tr>
                             <th className="show992">ID</th>
-                            <th className="show576">Data</th>
-                            <th>Vendedor</th>
-                            <th className="show992">Visitas</th>
-                            <th className="show992">Vendas</th>
+                            <th className="show576">Date</th>
+                            <th>Seller</th>
+                            <th className="show992">Visits</th>
+                            <th className="show992">Sales</th>
                             <th>Total</th>
-                            <th>Notificar</th>
+                            <th>Notificate</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td className="show992">#341</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Annah</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
                             <td>R$ 55300.00</td>
@@ -62,12 +62,12 @@ function SalesCard() {
                             </td>
                         </tr>
                         <tr>
-                            <td className="show992">#341</td>
+                            <td className="show992">#745</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Josh</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
+                            <td>R$ 27600.00</td>
                             <td>
                                 <div className="dsmeta-red-btn-container">
                                     <NotificationButton />
@@ -75,12 +75,12 @@ function SalesCard() {
                             </td>
                         </tr>
                         <tr>
-                            <td className="show992">#341</td>
+                            <td className="show992">#927</td>
                             <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
+                            <td>Robert</td>
                             <td className="show992">15</td>
                             <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
+                            <td>R$ 76150.00</td>
                             <td>
                                 <div className="dsmeta-red-btn-container">
                                     <NotificationButton />
